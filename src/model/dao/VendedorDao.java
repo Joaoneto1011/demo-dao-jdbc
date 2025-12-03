9d9a878ad0e2b2eba03be629da138c565f890af7
Joao Neto <neto31510@gmail.com>
2025-12-02 21:22:48 -0300

    Inserindo corretamente a interface VendedorDao

--- src/model/dao/VendedorDao.java
+++ src/model/dao/VendedorDao.java
@@ -1,5 +1,17 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Vendedor;
+
 public interface VendedorDao {
+	
+	void insert (Vendedor obj);
+	void update (Vendedor obj);
+	void deleteById (Vendedor obj);
+	
+	Vendedor findById(Integer id);
+	
+	List<Vendedor> findAll();
 
 }
