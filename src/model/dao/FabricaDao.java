21be4bde1b4f29a26fc73e923ad97094919d2187
Joao Neto <neto31510@gmail.com>
2025-12-02 22:05:21 -0300

    Implementando a funcao findById

--- src/model/dao/FabricaDao.java
+++ src/model/dao/FabricaDao.java
@@ -1,12 +1,13 @@
 package model.dao;
 
+import db.DB;
 import model.dao.implementacao.VendedorDaoJDBC;
 
 public class FabricaDao {
 	
 	public static VendedorDao createVendedorDao() {
 		
-		return new VendedorDaoJDBC();
+		return new VendedorDaoJDBC(DB.getConnection());
 	}
 
 }
--- src/model/dao/implementacao/VendedorDaoJDBC.java
+++ src/model/dao/implementacao/VendedorDaoJDBC.java
@@ -1,11 +1,25 @@
 package model.dao.implementacao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class VendedorDaoJDBC implements VendedorDao {
+	
+	private Connection conn;
+	
+	public VendedorDaoJDBC(Connection conn) {
+		
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Vendedor obj) {
@@ -24,7 +38,49 @@ public class VendedorDaoJDBC implements VendedorDao {
 
 	@Override
 	public Vendedor findById(Integer id) {
-		return null;
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ? ");
+			
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if (rs.next()) {
+				Departamento departamento = new Departamento();
+				departamento.setId(rs.getInt("DepartmentId"));
+				departamento.setNome(rs.getString("DepName"));
+				
+				Vendedor vendedor = new Vendedor();
+				vendedor.setId(rs.getInt("Id"));
+				vendedor.setNome(rs.getString("Name"));
+				vendedor.setEmail(rs.getString("Email"));
+				vendedor.setSalarioBase(rs.getDouble("BaseSalary"));
+				vendedor.setDataNascimento(rs.getDate("BirthDate"));
+				vendedor.setDepartamento(departamento);
+				
+				return vendedor;
+			}
+			return null;
+		}
+		
+		catch (SQLException e) {
+			
+			throw new DbException(e.getMessage());
+		}
+		
+		finally {
+			
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
